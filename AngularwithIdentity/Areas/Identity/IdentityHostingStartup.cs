66904ed68fe610cbbc83fc6578e889ba29392012
shadab ahmed khan <MY_NAME@example.com>
2021-10-06 19:21:25 +0500

    Add project files.

---
+++ AngularwithIdentity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AngularwithIdentity.Data;
+using AngularwithIdentity.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AngularwithIdentity.Areas.Identity.IdentityHostingStartup))]
+namespace AngularwithIdentity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
